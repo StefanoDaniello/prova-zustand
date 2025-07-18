930d7223c39cda931d73d29c7777a6d6d16f4c2c
StefanoDaniello <stefanodaniello68@gmail.com>
2025-07-18 10:24:56 +0200

    add tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       lato server e divero da quallo lato client */}
       <body
         suppressHydrationWarning
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
         {children}
       </body>
---
+++ src/app/store.ts
@@ -0,0 +1,18 @@
+import {create} from "zustand";
+
+type CounterStore ={
+    count: number;
+    increase: () => void;
+    increaseAsync: ()  => Promise<void>;
+    decrease: () => void;
+}
+
+export const useCounterStore = create<CounterStore>((set) => ({
+    count: 0,
+    increase: () => set((state) => ({ count: state.count + 1 })),
+    increaseAsync: async () => {
+        await new Promise((resolve)=> setTimeout (resolve, 1000));
+        set((state) => ({ count: state.count + 1 }));
+    },
+    decrease: () => set((state) => ({ count: state.count - 1 })),
+}));
